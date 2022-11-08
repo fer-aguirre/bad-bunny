c5b777035bbebe98532efdd28574f874d7e87666
Fer Aguirre <kay_op@protonmail.com>
2022-11-08 17:14:57 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Bad Bunny',
+    version='0.1.0',
+    author='Fer Aguirre',
+    description='Analysis of Bad Bunny's songs.',
+    python_requires='>=3',
+    license='MIT License',
+    packages=find_packages(),
+)
